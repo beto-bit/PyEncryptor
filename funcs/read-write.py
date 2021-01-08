9be0e4373c6fc79b97428db26a1ea69f57ae51b5
beto-bit <76757367+beto-bit@users.noreply.github.com>
2021-01-08 15:38:14 -0600

    ✏Cambios en Escritura
    Básicamente rediseñé la función que se encarga de escribir nuevos documentos. Ahora, en vez de aceptar listas y hacer el trabajo de separación de caracteres, simplemente acepta un generador y escribe las líneas en el documento.

--- funcs/read-write.py
+++ funcs/read-write.py
@@ -6,12 +6,14 @@ diversos documentos.
 def reader(name_doc):
     """
     Esta función se encarga de crear un objeto generador con todos las líneas
-    del documento. Luego, retorna ese objeto.
-
-    El propósito de crear un generador con las LINEAS y no con los CARACTERES
-    es para equilibrar el número de pasos a realizar por la computadora, y 
-    la memoria RAM utilizada. De forma que se busca un equilibrio sano.
+    del documento. Luego, retorna ese objeto. Recibe como argumento el nombre
+    del documento del cual genera el objeto generador.
     """
+
+    # El propósito de crear un generador con las LINEAS y no con los CARACTERES
+    # es para equilibrar el número de pasos a realizar por la computadora, y 
+    # la memoria RAM utilizada. De forma que se busca un equilibrio sano.
+
     # Creando el generador
     def gen():
         with open(name_doc, 'r', encoding="utf-8") as f:
@@ -19,29 +21,14 @@ def reader(name_doc):
                 yield line
 
     return gen
-            
 
-
-
-def lines_writer(name_doc, lista):
+def writer(name_doc, gen):
     """
-    Esta función escribe en un documento tomando como argumento el nombre
-    del documento y la lista que contiene las diversas líneas del mismo.
-    Se encarga de que se inserten en líneas nuevas siempre (que sea necesario).
-    
-    En caso de que se genere un error debido a la introudcción de caracteres
-    incompatibles (y por ende un error en la contraseña) escribe los caracteres
-    usando bits y en 'utf-8'.
+    Esta función escribe en un documento (name_doc) las líneas que recibe de
+    parte del generador que se le pasa como argumento (gen).
     """
-    try:
-        with open(name_doc, 'w') as f:
-            for linea in lista:
-                if linea[-1] == "$":
-                    f.write(linea + "\n")
-                else:
-                    f.write(linea)
-    except:
-        with open(name_doc, 'wb') as f:
-            for linea in lista:
-                f.write(linea.encode('utf-8'))
+
+    with open(name_doc, "w", encoding="utf-8") as f:
+        for line in gen():
+            f.write(line)
 
