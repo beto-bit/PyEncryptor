d4bd91dbcfbf5ada0bc5a3eb048268d77b23a5c6
beto-bit <76757367+beto-bit@users.noreply.github.com>
2021-01-08 12:47:27 -0600

    📕 Cambios en Lectura
    Se ha cambiado la forma en que funciona la función que se encarga de leer el documento; ha sido sustituida por una que retorna un objeto generador en vez de una lista, lo que disminuye el consumo de memoria RAM.

--- funcs/read-write.py
+++ funcs/read-write.py
@@ -1,15 +1,26 @@
-def lines_generator(name_doc):
-    """
-    Esta función crea una lista con las líneas del documento.
-    """
-    with open(name_doc, 'r', encoding="utf-8") as f:
-        # Generar lista
-        lista = []
+"""
+Este módulo contiene las diversas funciones que permiten la lectoescritura de
+diversos documentos.
+"""
 
-        for linea in f:
-            lista.append(linea)
+def reader(name_doc):
+    """
+    Esta función se encarga de crear un objeto generador con todos las líneas
+    del documento. Luego, retorna ese objeto.
 
-        return lista
+    El propósito de crear un generador con las LINEAS y no con los CARACTERES
+    es para equilibrar el número de pasos a realizar por la computadora, y 
+    la memoria RAM utilizada. De forma que se busca un equilibrio sano.
+    """
+    # Creando el generador
+    def gen():
+        with open(name_doc, 'r', encoding="utf-8") as f:
+            for line in f:
+                yield line
+
+    return gen
+            
+
 
 
 def lines_writer(name_doc, lista):
