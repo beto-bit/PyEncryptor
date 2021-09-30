71c4002b9f2c00451c1a205a1ebfa38aab175a75
beto-bit <76757367+beto-bit@users.noreply.github.com>
2021-09-30 16:10:49 -0600

    Password Encryption
    Created functions to encrypt/decrypt data using a password. Also changed name of arguments in crypto.py for more readability.

--- crypto/crypto.py
+++ crypto/crypto.py
@@ -4,13 +4,13 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
-def encrypt_file(key: bytes, filename: str) -> None:
+def encrypt_file(key: bytes, filepath: str) -> None:
     """Encrypts a file."""
     # Using the generated key
     fernet = Fernet(key)
 
     # Open the original file
-    with open(filename, 'rb') as file:
+    with open(filepath, 'rb') as file:
         original = file.read()
         file.close()
 
@@ -18,23 +18,23 @@ def encrypt_file(key: bytes, filename: str) -> None:
     encrypted = fernet.encrypt(original)
 
     # Writing the encripted file
-    with open(filename, 'wb') as encrypted_file:
+    with open(filepath, 'wb') as encrypted_file:
         encrypted_file.write(encrypted)
         encrypted_file.close()
 
-def decrypt_file(key: bytes, filename: str) -> None:
+def decrypt_file(key: bytes, filepath: str) -> None:
     """Decrypts a file."""
     fernet = Fernet(key)
 
     # Open the file
-    with open(filename, 'rb') as enc_file:
+    with open(filepath, 'rb') as enc_file:
         encrypted = enc_file.read()
         enc_file.close()
 
     decrypted = fernet.decrypt(encrypted)
 
     # Write the decripted data
-    with open(filename, 'wb') as dec_file:
+    with open(filepath, 'wb') as dec_file:
         dec_file.write(decrypted)
         dec_file.close()
 
--- crypto/operations.py
+++ crypto/operations.py
@@ -21,6 +21,8 @@ def read_file(filepath) -> bytes:
 
     return content
 
+
+# Basic encryption/decryption funcs
 def basic_encryption(filepath: str, override=False) -> bytes:
     """Encrypts a file using a generated key, writes the key in the
     same folder as the file and then returns the key"""
@@ -38,7 +40,6 @@ def basic_encryption(filepath: str, override=False) -> bytes:
 
     return key
     
-
 def basic_decryption(filepath: str) -> None:
     """Decrypts a file using the generated filekey."""
     filepath = Path(filepath)
@@ -49,3 +50,37 @@ def basic_decryption(filepath: str) -> None:
     decrypt_file(key, str(filepath))
 
 
+# Encryption/decryption with password
+def encryption_with_psw(filepath: str, psw: bytes, override=False) -> None:
+    """Encrypts a file using a given password, then storing the
+    salt in a separate file, in order to replicate the key."""
+    filepath = Path(filepath)
+    path = get_path(filepath)
+    
+    # Generate key
+    key = generate_key_from_psw(    
+        psw, 
+        generate_saltfile(path)
+    )
+
+    # Override Options
+    if override:
+        encrypt_file(key, str(filepath))
+    else:
+        new_filepath = path + filepath.stem + '_crypted' + filepath.suffix
+        copyfile(str(filepath), new_filepath)
+        encrypt_file(key, new_filepath)
+
+def decryption_with_psw(filepath: str, psw: bytes) -> None:
+    """Decrypts a file using a given password."""
+    path = get_path(Path(filepath))
+
+    # Generate again the key
+    key = generate_key_from_psw(
+        psw,
+        read_file(path + 'salt.key')
+    )
+
+    # Decrypt
+    decrypt_file(key, filepath)
+    
