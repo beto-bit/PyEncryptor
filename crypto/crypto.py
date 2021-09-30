ed34aab216fa0e8f901afb52c02630cf51952ad5
beto-bit <76757367+beto-bit@users.noreply.github.com>
2021-09-29 22:00:09 -0600

    Basic Cryptography
    Created simple functions to edit files and encrypt them.

--- crypto/crypto.py
+++ crypto/crypto.py
@@ -1,1 +1,36 @@
 from cryptography.fernet import Fernet
+
+def encrypt_file(key: bytes, path: str):
+    """Encrypts a specified file."""
+    # Using the generated key
+    fernet = Fernet(key)
+
+    # Open the original file
+    with open(path, 'rb') as file:
+        original = file.read()
+        file.close()
+
+    # Encrypting
+    encrypted = fernet.encrypt(original)
+
+    # Writing the encripted file
+    with open(path, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+        encrypted_file.close()
+
+
+def decrypt_file():
+    pass
+
+def generate_key() -> bytes:
+    """Simple encapsulator to generate key."""
+    return Fernet.generate_key()
+
+def generate_filekey(path='') -> None:
+    """Generates a filekey in the specified path."""
+    key = Fernet.generate_key()
+
+    # Generate the filekey
+    with open(path + 'filekey.key', 'wb') as filekey:
+        filekey.write(key)
+        filekey.close()
